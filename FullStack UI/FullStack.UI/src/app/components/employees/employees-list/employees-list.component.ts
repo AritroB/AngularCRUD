7cac31928a7b4848ce7a7c6e490bbe13b0c46493
AritroB <77176367+AritroB@users.noreply.github.com>
2023-05-26 14:38:10 -0400

    finished connecting to API

--- FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -8,35 +9,18 @@ import { Employee } from 'src/app/models/employee.model';
 })
 export class EmployeesListComponent implements OnInit{
 
-  employees: Employee[] = [
-    {
-      id: "123abc-13fg32-34fvr3",
-      name: "John Doe",
-      email: "johndoe@example.com",
-      phone: 1234567890,
-      salary: 5000,
-      department: "IT"
-    },
-    {
-      id: "456def-98ht76-21vbg7",
-      name: "Jane Smith",
-      email: "janesmith@example.com",
-      phone: 9876543210,
-      salary: 6000,
-      department: "Finance"
-    },
-    {
-      id: "789ghi-54ko98-76nbt4",
-      name: "Alex Johnson",
-      email: "alexjohnson@example.com",
-      phone: 5555555555,
-      salary: 4500,
-      department: "Marketing"
-    }
-  ];
-  constructor() { }
+  employees: Employee[] = [];
+  constructor(private employeesService: EmployeesService) { }
 
   ngOnInit(): void {
-      this.employees.push();
+    this.employeesService.getAllEmployees()
+    .subscribe({
+      next: (employees) => {
+        this.employees = employees;
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    })
   }
 }
